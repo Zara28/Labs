6cad074a888c76946e7f26cc9ff8a1ab242f657a
Zara28 <a.bestugeva2003@gmail.com>
2021-10-17 21:30:21 +0400

    laba8

---
+++ Laba8/game_2d_arr/Header.h
@@ -0,0 +1,22 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 
+#include "stdio.h"
+#define WIN32_LEAN_AND_MEAN            
+#include <stdlib.h>
+
+#include "framework.h"
+
+void moveDown();
+void moveToLeft();
+void moveUp();
+void moveToRight();
+void Create_wall_left();
+
+void Create_wall_right();
+void Destroy_wall_right();
+void Destroy_wall(int ras);
+void Destroy_wall_D();
+void Double();
+void Again();
+void to_end();
+void DrawField(HDC hdc);
